b19c386897ea2ce37a42087b8147067b3fbdef3f
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-16 02:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 function fn(){
     var Name = document.getElementById('name').value;
   var year = document.getElementById('year').value;
-    document.getElementById('url').innerHTML += '<br>'+"Name="   +Name+"&"+"Year="+year ;
+    document.getElementById('url').innerHTML += '<br>'+"https://localhost:8080/"+"Name="   +Name+"&"+"Year="+year ;
 }
